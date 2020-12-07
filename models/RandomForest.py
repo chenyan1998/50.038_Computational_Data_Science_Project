21948e29a01e1bdcbdbb11b8fc5dd1ea980f4fd9
luyingpang <pangly1212@gmail.com>
2020-12-07 20:47:32 +0800

    RandomForest

---
+++ models/RandomForest.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import DataProcessor as dp
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.externals import joblib
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+
+data_df = pd.read_csv('./newdata/2019.csv')
+
+# visualise how balance our dataset is
+sns.countplot(x="Hit", data=data_df, palette="muted")
+
+X_train, X_test, y_train, y_test = train_test_split(data_df.drop(['Hit'],axis=1), 
+                                                    data_df['Hit'],
+                                                    test_size=0.3,
+                                                    random_state=0) 
+# normalise dataset
+x_scaler = MinMaxScaler() 
+x_scaler.fit(X_train)
+X_train_norm = x_scaler.transform(X_train)
+X_test_norm = x_scaler.transform(X_test)
+
+#%% Define and train the model
+RF = RandomForestClassifier()#n_estimators = 50) # Define the model
+RF.fit(X_train_norm, y_train) 
+train_score = RF.score(X_train,y_train)
+
+#%% Predicting labels and evaluate
+y_pred = RF.predict(X_test_norm) 
+report,rocfig =dp.evaluate_on_training_set(y_test, y_pred) 
+pred_fig=dp.plot_pred_original(y_pred,y_test,'RandomForest')
+report['training score']=train_score
+
+# save predict result
+rocfig.savefig('./result/RandomForest/RFroc.png')
+print(report)
+with open('./result/RandomForest/RFreport', 'w') as f:
+    [f.write('{0}:\n{1}\n'.format(key, value)) for key, value in report.items()]
+pred_fig.savefig('./result/RandomForest/RFpred.png')
+
+#%%
+joblib.dump(RF, './trainedModel/RF.sav')
